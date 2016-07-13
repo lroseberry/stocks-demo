ad119c983c9aec6e8d6265fe6a63eb041e05e22e
Bob Basmaji <rjbx@users.noreply.github.com>
2016-07-13 11:36:19 -0400

    remove files

--- src/main/java/com/github/rjbx/demo/stocks/model/XMLDomainObject.java
+++
@@ -1,7 +0,0 @@
-package com.github.rjbx.demo.stocks.model;
-
-/**
- * A marker class the indicates the implementing class was created from an XML instance.
- */
-public interface XMLDomainObject {
-}
