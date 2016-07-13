031f419b6200bc68731a4e7b0e63b07fea7072ee
Bob Basmaji <rjbx@users.noreply.github.com>
2016-07-13 11:49:14 -0400

    remove files

--- src/main/java/com/github/rjbx/demo/stocks/model/DatabaseAccessObject.java
+++
@@ -1,7 +0,0 @@
-package com.github.rjbx.demo.stocks.model;
-
-/**
- * A marker class that indicates the implementing class is a database access object.
- */
-public interface DatabaseAccessObject {
-}
--- src/main/java/com/github/rjbx/demo/stocks/services/ServiceType.java
+++
@@ -1,7 +0,0 @@
-package com.github.rjbx.demo.stocks.services;
-
-/**
- * Enum representing the possible service type instances returned by the factory method.
- * @author Bob Basmaji
- */
-public enum ServiceType {BASIC, DATABASE};
